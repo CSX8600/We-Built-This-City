49c7d57a650f191cabd7cbb086dcb13c10fb6bfc
RavenholmZombie <supersoftware1234@gmail.com>
2020-12-19 12:48:53 -0500

    First commit

---
+++ src/main/java/rz/mesabrook/wbtc/Main.java
@@ -0,0 +1,31 @@
+package rz.mesabrook.wbtc;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
+public class Main
+{
+    public static final String MODID = "wbtc";
+    public static final String NAME = "We Built This City";
+    public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("We Built This City on Rock n Roll!");
+    }
+}

df40b29fbb00b4bdd845fce86682fd7e2235b41b
RavenholmZombie <supersoftware1234@gmail.com>
2021-01-13 17:55:39 -0500

    sods
    sods

--- src/main/java/rz/mesabrook/wbtc/init/ModItems.java
+++ src/main/java/rz/mesabrook/wbtc/init/ModItems.java
@@ -3,8 +3,11 @@ package rz.mesabrook.wbtc.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
 import rz.mesabrook.wbtc.items.drinks.WBTC_Beverage;
+import rz.mesabrook.wbtc.items.weapons.ItemSod;
 
 public class ModItems 
 {
@@ -12,4 +15,11 @@ public class ModItems
 	
 	// Test Drink
 	//public static final Item TEST_WATER = new WBTC_Beverage("test_water");
+	
+	// Sods
+	public static final Item WOOD_SOD = new ItemSod("wood_sod", ToolMaterial.WOOD);
+	public static final Item STONE_SOD = new ItemSod("stone_sod", ToolMaterial.STONE);
+	public static final Item IRON_SOD = new ItemSod("iron_sod", ToolMaterial.IRON);
+	public static final Item GOLD_SOD = new ItemSod("gold_sod", ToolMaterial.GOLD);
+	public static final Item DIAMOND_SOD = new ItemSod("diamond_sod", ToolMaterial.DIAMOND);
 }
---
+++ src/main/java/rz/mesabrook/wbtc/items/weapons/ItemSod.java
@@ -0,0 +1,26 @@
+package rz.mesabrook.wbtc.items.weapons;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemSword;
+import rz.mesabrook.wbtc.Main;
+import rz.mesabrook.wbtc.init.ModItems;
+import rz.mesabrook.wbtc.util.IHasModel;
+
+public class ItemSod extends ItemSword implements IHasModel
+{
+	public ItemSod(String name, ToolMaterial material)
+	{
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.COMBAT);
+		
+		ModItems.ITEMS.add(this);
+	}
+	
+	@Override
+	public void registerModels()
+	{
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+}

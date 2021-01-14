820aa12aeb3f0cf18e7939d57134fb983f88a3f4
RavenholmZombie <supersoftware1234@gmail.com>
2021-01-13 19:54:29 -0500

    Finished sods

--- src/main/java/rz/mesabrook/wbtc/init/ModItems.java
+++ src/main/java/rz/mesabrook/wbtc/init/ModItems.java
@@ -6,6 +6,7 @@ import java.util.List;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
 import rz.mesabrook.wbtc.items.drinks.WBTC_Beverage;
 import rz.mesabrook.wbtc.items.weapons.ItemSod;
 
@@ -13,13 +14,17 @@ public class ModItems
 {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
-	// Test Drink
-	//public static final Item TEST_WATER = new WBTC_Beverage("test_water");
+	// Sod Materials
+	public static final ToolMaterial SOD_WOOD = EnumHelper.addToolMaterial("wbtc_wood", 0, 30, 1.0F, -2.0F, 8);
+	public static final ToolMaterial SOD_STONE = EnumHelper.addToolMaterial("wbtc_stone", 1, 66, 2.0F, -0.5F, 5);
+	public static final ToolMaterial SOD_IRON = EnumHelper.addToolMaterial("wbtc_iron", 2, 125, 3.0F, -1.0F, 8);
+	public static final ToolMaterial SOD_GOLD = EnumHelper.addToolMaterial("wbtc_gold", 0, 16, 1.0F, -2.0F, 11);
+	public static final ToolMaterial SOD_DIAMOND = EnumHelper.addToolMaterial("wbtc_diamond", 3, 780, 4.0F, -0.5F, 5);
 	
 	// Sods
-	public static final Item WOOD_SOD = new ItemSod("wood_sod", ToolMaterial.WOOD);
-	public static final Item STONE_SOD = new ItemSod("stone_sod", ToolMaterial.STONE);
-	public static final Item IRON_SOD = new ItemSod("iron_sod", ToolMaterial.IRON);
-	public static final Item GOLD_SOD = new ItemSod("gold_sod", ToolMaterial.GOLD);
-	public static final Item DIAMOND_SOD = new ItemSod("diamond_sod", ToolMaterial.DIAMOND);
+	public static final Item WOOD_SOD = new ItemSod("wood_sod", SOD_WOOD);
+	public static final Item STONE_SOD = new ItemSod("stone_sod", SOD_STONE);
+	public static final Item IRON_SOD = new ItemSod("iron_sod", SOD_IRON);
+	public static final Item GOLD_SOD = new ItemSod("gold_sod", SOD_GOLD);
+	public static final Item DIAMOND_SOD = new ItemSod("diamond_sod", SOD_DIAMOND);
 }
--- src/main/java/rz/mesabrook/wbtc/items/weapons/ItemSod.java
+++ src/main/java/rz/mesabrook/wbtc/items/weapons/ItemSod.java
@@ -1,7 +1,17 @@
 package rz.mesabrook.wbtc.items.weapons;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import rz.mesabrook.wbtc.Main;
 import rz.mesabrook.wbtc.init.ModItems;
 import rz.mesabrook.wbtc.util.IHasModel;
@@ -16,6 +26,15 @@ public class ItemSod extends ItemSword implements IHasModel
 		setCreativeTab(CreativeTabs.COMBAT);
 		
 		ModItems.ITEMS.add(this);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag flag)
+	{
+		tooltip.add(TextFormatting.RED + "noo you can't just break a sword in half and call it a sod!");
+		tooltip.add(TextFormatting.AQUA + "hehe sod go swish slash");
+		super.addInformation(stack, world, tooltip, flag);
 	}
 	
 	@Override

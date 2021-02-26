401cc2c9871f4a3db0d879e8ea9aa3e322bc9e9e
RavenholmZombie <supersoftware1234@gmail.com>
2021-02-25 22:09:16 -0500

    Update BlockPlaque.java

--- src/main/java/rz/mesabrook/wbtc/blocks/BlockPlaque.java
+++ src/main/java/rz/mesabrook/wbtc/blocks/BlockPlaque.java
@@ -10,6 +10,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
